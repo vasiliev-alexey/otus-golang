bd1df34c35e92a2cc50dd103502369b892029049
vasiliev-alexey <alex_v81@mail.ru>
2021-01-23 17:50:34 +0300

    ДЗ 6

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,4 +1,5 @@
 package hw06_pipeline_execution //nolint:golint,stylecheck
+import "log"
 
 type (
 	In  = <-chan interface{}
@@ -9,6 +10,35 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here
-	return nil
+	outCh := in
+	for _, stage := range stages {
+		outCh = func(in In) (out Out) {
+			bindCh := make(Bi)
+
+			go func() {
+				defer close(bindCh)
+
+				for {
+					select {
+					case <-done:
+						log.Println("graceful shutdown processing")
+						return
+					case v, ok := <-in:
+						if !ok {
+							return
+						}
+						select {
+						case <-done:
+						case bindCh <- v:
+							log.Println("перекладываем значение в связующий канал")
+						}
+					}
+				}
+			}()
+			log.Println("Вызываем трансформер на связующем канале")
+			return stage(bindCh)
+		}(outCh)
+	}
+
+	return outCh
 }
